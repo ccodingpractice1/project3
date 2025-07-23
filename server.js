ce87d902fe6fe91e1f2826c4b79873b3b335e5b7
christinel <christinelahens@liberty.com>
2025-07-23 17:32:34 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const da = require("./data-access");
+
+
 const express = require('express');
 const path = require('path');  // for handling file paths
 
 const app = express();
-const port = process.env.PORT || 4000;  // use env var or default to 4000
+const port = process.env.PORT || 3000;  // use env var or default to 4000
 
 // Set the static directory to serve files from
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+app.get("/customers", async (req, res) => {
+    const cust = await da.getCustomers();
+    res.send(cust);    
 });
