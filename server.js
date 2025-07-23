ba942cb9bb162220fb9dacb46497ee2792587531
christinel <christinelahens@liberty.com>
2025-07-23 17:43:10 -0400

    completed stage03 add error handling

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
+
 app.get("/customers", async (req, res) => {
-    const cust = await da.getCustomers();
-    res.send(cust);    
+    const [cust, err] = await da.getCustomers();
+    if(cust){
+        res.send(cust);
+    }else{
+        res.status(500);
+        res.send(err);
+    }   
 });
+
+
+
