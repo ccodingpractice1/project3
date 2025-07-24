f9e083d20990e418e465de3295a564482ae5b431
christinel <christinelahens@liberty.com>
2025-07-23 21:52:56 -0400

    completed stage05 add post endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const da = require("./data-access");
+const bodyParser = require('body-parser');
 
 
 const express = require('express');
@@ -6,6 +7,8 @@ const path = require('path');  // for handling file paths
 
 const app = express();
 const port = process.env.PORT || 4000;  // use env var or default to 4000
+
+app.use(bodyParser.json());
 
 // Set the static directory to serve files from
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,4 +38,23 @@ app.get("/reset", async (req, res) => {
     }   
 });
 
+app.post('/customers', async (req, res) => {
+    const newCustomer = req.body;
+    if (newCustomer === null) {
+        res.status(400);
+        res.send("missing request body");
+    } else {
+        // return array format [status, id, errMessage]
+        const [status, id, errMessage] = await da.addCustomer(newCustomer);
+        if (status === "success") {
+            res.status(201);
+            let response = { ...newCustomer };
+            response["_id"] = id;
+            res.send(response);
+        } else {
+            res.status(400);
+            res.send(errMessage);
+        }
+    }
+});
 
